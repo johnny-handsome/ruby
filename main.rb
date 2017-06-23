0ad7f37b6004ab13a82b2cb77efa7f86806e3c4f
Yuriy Bahur <yuriybahur@gmail.com>
2017-06-23 22:39:39 +0300

    third

--- main.rb
+++ main.rb
@@ -1,21 +1,20 @@
-class Message
-	def message
-		print 'Random num'
-			str = gets
-			str = str.to_f
-			case str
-				when 1
-					puts 'got the job'
-				when 2
-					puts 'i m in trouble'
-				when 3
-					puts 'fuck with many women'
-				else
-					for i in 1..10
-						puts 'hui'
-				end
+class SeaBattle
+	def SeaBattle.printMes1
+		print "Enter the location of ship (from 1 to 10):"
+		strLocationShip = gets.to_i
+		arr = ["*","*","*","*","*","*","*","*","*","*"].join()
+		arr[strLocationShip-1]="O"
+		puts "#{arr}"
+		puts "Your ship is ready to fight!"
+		print "Shoot to thrill (from 1 to 10):"
+		strShootLocation = gets.to_i
+		if strLocationShip == strShootLocation
+			puts "You are fuckin awesome! Win!!!!"
+		else
+			arr[strShootLocation-1]="X"
+			puts "#{arr}"
+			puts "Try one time more,bro"
+		end
+	end
 end
-end
-end
-messageOutput = Message.new
-messageOutput.message
+SeaBattle.printMes1
