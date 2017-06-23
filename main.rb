f273436889318be3c0a0f26a02b068fd592f749b
Yuriy Bahur <yuriybahur@gmail.com>
2017-06-24 01:59:38 +0300

    ghg

--- main.rb
+++ main.rb
@@ -1,20 +1,62 @@
 class SeaBattle
-	def SeaBattle.printMes1
-		print "Enter the location of ship (from 1 to 10):"
-		strLocationShip = gets.to_i
-		arr = ["*","*","*","*","*","*","*","*","*","*"].join()
-		arr[strLocationShip-1]="O"
-		puts "#{arr}"
-		puts "Your ship is ready to fight!"
-		print "Shoot to thrill (from 1 to 10):"
-		strShootLocation = gets.to_i
-		if strLocationShip == strShootLocation
-			puts "You are fuckin awesome! Win!!!!"
-		else
-			arr[strShootLocation-1]="X"
-			puts "#{arr}"
-			puts "Try one time more,bro"
+	def initialize
+		@arr = ["*","*","*","*","*","*","*","*","*","*"]
+	end
+	def show 
+		for i in @arr.length-1
+			print @arr[i]
 		end
 	end
+	def gamee
+		print "Enter the location of ship (from 1 to 10):"
+		strLocationShip = gets.to_i
+		@arr[strLocationShip-1]="O"
+		show 
+		puts "Your ship is ready to fight!"
+		for i in 0..@arr.length-1
+			print "Sot to thrill (from 1 to 10):"
+			strShootLocation = gets.to_i
+			if strLocationShip == strShootLocation
+				puts "You are fuckin awesome! Win!!!!"
+				return
+			else
+				@arr[strShootLocation-1]="X"
+				show 
+				puts "Try one time more,bro"
+			end
+		end
+		print "lol"
+	end
 end
-SeaBattle.printMes1
+output = SeaBattle.new
+output.gamee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
